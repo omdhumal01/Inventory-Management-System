0acfd7decbca182f8ccb6a151ce4a8d9cf965951
omdhumal01 <omd68630@gmail.com>
2026-07-23 21:11:55 +0530

    done

--- frontend/src/services/api/axios.js
+++ frontend/src/services/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+  baseURL: "https://inventory-management-system-9h83.onrender.com/api",
 });
 
 
